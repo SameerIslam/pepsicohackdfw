9d70792457acb7063152a24cef8c1ce7bd3c7382
Aarian <aarianahsan@gmail.com>
2022-09-24 15:38:52 -0500

    styled the grid

--- pepsico-app/src/frontend/main_page.js
+++ pepsico-app/src/frontend/main_page.js
@@ -1,9 +1,4 @@
 import { signIn } from "../backend/auth";
-
-import Navbar from "./components/navBar";
-
-
-
 import { Board } from "../backend/Board_Model";
 import { getBoard } from "../backend/crud_functions";
 import { useState } from "react";
@@ -15,46 +10,36 @@ const arrOfObj = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
 
-
 export function MainPage() {
+  const [board, setBoard] = useState();
 
-  
-    const [board, setBoard] = useState(); 
+  async function getResponse() {
+    const a = await signIn();
+    const userBoard = await getBoard(a.userId);
+    setBoard(userBoard);
+  }
+  const handleClick = (event) => {
+    getResponse();
+  };
 
-    async function getResponse(){
-        const a = await signIn();
-        const userBoard = await getBoard(a.userId);
-        setBoard(userBoard);
-    }
-    const handleClick = (event) =>{
-        getResponse();
-    }
+  async function testRandoms() {
+    board.revealRandom(5);
+    console.log(board);
+  }
 
-    async function testRandoms(){
-        board.revealRandom(5);
-        console.log(board);
-    }
+  const handleClick2 = () => {
+    testRandoms();
+  };
+  return (
+    <div>
+      <Navbar />
+      <Grid arrOfObj={arrOfObj} />
+      <div className="container">
+        <button onClick={handleClick}>Main page</button>
+      </div>
 
-    const handleClick2 = () => {
-        testRandoms();
-    }
-    return(
-        <div>
-
-            <Navbar />
-             <Grid arrOfObj={arrOfObj} />
-            <div className="container">
-                <button onClick={handleClick}>Main page</button>
-            </div>
-            
-
-
-
-            <button onClick={handleClick}>Main page</button>
-            <button onClick={handleClick2}>test page</button>
-        </div>
-    );
+      <button onClick={handleClick}>Login in</button>
+      <button onClick={handleClick2}>test page</button>
+    </div>
+  );
 }
-
-
-
