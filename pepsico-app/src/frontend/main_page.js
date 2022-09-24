e912bb1928a2da9e16c7b96c418310972b0d7e33
SameerIslam <islamsameer91@gmail.com>
2022-09-24 14:45:05 -0500

    added login button to navbar

--- pepsico-app/src/frontend/main_page.js
+++ pepsico-app/src/frontend/main_page.js
@@ -1,20 +1,23 @@
-//import { signIn } from "../backend/auth";
-import Navbar from "./components/navBar.js";
+import { signIn } from "../backend/auth";
+import Navbar from "./components/navBar";
 
-export function MainPage() {
-  async function getResponse() {
-    //const a = await signIn();
-  }
 
-  const handleClick = (event) => {
-    getResponse();
-  };
-  return (
-    <>
-      <Navbar />
-      <div className="container">
-        <button onClick={handleClick}>Main page</button>
-      </div>
-    </>
-  );
+export function MainPage(){
+    async function getResponse(){
+        const a = await signIn();
+    }
+
+    const handleClick = (event) =>{
+        getResponse();
+    }
+    return(
+        
+        <>
+            <Navbar />
+            <div className="container">
+                <button onClick={handleClick}>Main page</button>
+            </div>
+ 
+        </>
+    );
 }
