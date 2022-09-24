01b01b10e22f96157144cc0cc8879d93af952d09
Abrar Zaman <abrarzaman2003@gmail.com>
2022-09-24 01:35:57 -0500

    testing auth code

--- pepsico-app/src/frontend/main_page.js
+++ pepsico-app/src/frontend/main_page.js
@@ -1,6 +1,15 @@
-export function MainPage(){
-    return(
-        <h1>Main page</h1>
-    );
+import { signIn } from "../backend/auth";
 
+
+export function MainPage(){
+    async function getResponse(){
+        const a = await signIn();
+    }
+
+    const handleClick = (event) =>{
+        getResponse();
+    }
+    return(
+        <button onClick={handleClick}>Main page</button>
+    );
 }
