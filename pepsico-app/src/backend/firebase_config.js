8fa6af3bbeb1ec9ce3ea0fda83e811cc72d20049
Abrar Zaman <abrarzaman2003@gmail.com>
2022-09-24 00:55:38 -0500

    initialized firebase config

---
+++ pepsico-app/src/backend/firebase_config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjvPtKg55tcGcfxCL3bPubg5ztElMnTjc",
+  authDomain: "pepsico-backend.firebaseapp.com",
+  projectId: "pepsico-backend",
+  storageBucket: "pepsico-backend.appspot.com",
+  messagingSenderId: "991247222112",
+  appId: "1:991247222112:web:c0a8a8f1ed073ccb3c4a05",
+  measurementId: "G-L84ZGHP7V3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
