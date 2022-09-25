28f69df8cc614dbbc95efe28366da589e1d48fef
Aarian <aarianahsan@gmail.com>
2022-09-24 23:46:32 -0500

    removed bloat & jpeg, & updated enter code button

--- pepsico-app/src/frontend/components/textBox.js
+++ pepsico-app/src/frontend/components/textBox.js
@@ -1,40 +1,38 @@
-import React from 'react'
-import styles from "../styles/styles.css";
-import { useState } from 'react';
-import { validate } from '../../backend/crud_functions';
+import React from "react";
+import { useState } from "react";
+import { validate } from "../../backend/crud_functions";
 
 export default function TextBox(props) {
   const [value, setValue] = useState("");
 
-  async function validation(){
+  async function validation() {
     const arr = value.split("-");
     const num = await validate(arr[0], arr[1]);
     props.testRandoms(num);
     console.log(num);
   }
 
-  const handleClick = () =>{
+  const handleClick = () => {
     //var text = e.target.value;
-    
-    
-    validation();
-  }
 
-  const onChange = (e) =>{
+    validation();
+  };
+
+  const onChange = (e) => {
     setValue(e.target.value);
-  }
+  };
   return (
     <div class="tb">
-        
-            <input id = "couponInput" type = "text" placeholder='Enter Code Here' onChange={onChange} value={value}></input>
-            <button id = "textButton" onClick={handleClick}>
-                <span className="font-link">
-                    Enter Code
-                </span>
-            </button>
-        
+      <input
+        id="couponInput"
+        type="text"
+        placeholder="Enter Code Here"
+        onChange={onChange}
+        value={value}
+      ></input>
+      <button id="textButton" onClick={handleClick}>
+        <span className="font-link">Submit Code</span>
+      </button>
     </div>
-    
-
-  )
+  );
 }
--- pepsico-app/src/frontend/main_page.js
+++ pepsico-app/src/frontend/main_page.js
@@ -1,5 +1,4 @@
 import { signIn } from "../backend/auth";
-import { Board } from "../backend/Board_Model";
 import { getBoard, updateBoard } from "../backend/crud_functions";
 import { useEffect, useState } from "react";
 
@@ -9,10 +8,6 @@ import TextBox from "./components/textBox.js";
 import Chester from "../img/thumbs-up-chester-cheetah.png";
 import oasis from "../img/oasispano.png";
 
-const arrOfObj = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
-
 export function MainPage() {
   const [board, setBoard] = useState();
   const [boardVals, setBoardVals] = useState([]);
@@ -20,7 +15,7 @@ export function MainPage() {
 
   useEffect(() => {
     console.log(board);
-  }, [boardVals]);
+  });
   async function getResponse() {
     const a = await signIn();
     await setUser(a);
@@ -39,9 +34,6 @@ export function MainPage() {
     await updateBoard(user.userId, board);
   }
 
-  const handleClick2 = () => {
-    testRandoms();
-  };
   return (
     <div>
       <Navbar func={getResponse} />
@@ -50,6 +42,7 @@ export function MainPage() {
         <div class="card">
           <p>and act like ion know nobody</p>
         </div>
+
         <Grid arrOfObj={boardVals} />
         <div className="chestah">
           <img src={Chester} alt="Chester" width="250 px" />
