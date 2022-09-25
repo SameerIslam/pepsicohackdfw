0c9dc88d0f2f3f35775e31087c5ac90438b94e4e
Aarian <aarianahsan@gmail.com>
2022-09-25 02:31:04 -0500

    Trying to add a border to grif

--- pepsico-app/src/frontend/components/grid.js
+++ pepsico-app/src/frontend/components/grid.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Tile } from "./tile";
 
-var z = {zIndex:-1};
+var z = { zIndex: -1 };
 
 export default function Grid(props) {
-  if (props.load){
-    z = {zIndex:2};
+  if (props.load) {
+    z = { zIndex: 2 };
   }
   const cells = props.arrOfObj.map((obj) => <Tile obj={obj}></Tile>);
 
-  return <div className="container1" style={z}>{cells}</div>;
+  return (
+    <div className="gridBorder">
+      <div className="container1" style={z}>
+        {cells}
+      </div>
+    </div>
+  );
 }
